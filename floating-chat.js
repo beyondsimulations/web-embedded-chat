6504cb9070c2b8fc04efa7eae6c6fa8aa5e82730
Tobias Vlcek <vlcek@beyondsimulations.com>
2025-08-18 13:10:11 +0200

    Move code block parsing before other Markdown rules

--- floating-chat.js
+++ floating-chat.js
@@ -1214,13 +1214,13 @@ class UniversalChatWidget {
       .replace(/>/g, "&gt;");
 
     return escaped
+      .replace(/```([\s\S]*?)```/g, "<pre><code>$1</code></pre>")
       .replace(/^### (.*$)/gm, "<h3>$1</h3>")
       .replace(/^## (.*$)/gm, "<h2>$1</h2>")
       .replace(/^# (.*$)/gm, "<h1>$1</h1>")
       .replace(/\*\*(.*?)\*\*/g, "<strong>$1</strong>")
       .replace(/\*(.*?)\*/g, "<em>$1</em>")
       .replace(/`([^`]+)`/g, "<code>$1</code>")
-      .replace(/```([\s\S]*?)```/g, "<pre><code>$1</code></pre>")
       .replace(/\n/g, "<br>");
   }
 
